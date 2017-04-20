ed3ed56ba525b59115f8aac27168eaafd902539c
Edison Cury Neto <ecury@br.ibm.com>
2017-04-20 10:45:09 -0300

    changes on app.js

--- app.js
+++ app.js
@@ -1,28 +1,141 @@
-/*eslint-env node*/
+var express = require('express'),
+    cfenv = require('cfenv'),
+    routes = require('./routes'),
+    http = require('http'),
+    path = require('path'),
+    fs = require('fs'),
+    bodyParser = require('body-parser'),
+    methodOverride = require('method-override'),
+    errorHandler = require('errorhandler');
 
-//------------------------------------------------------------------------------
-// node.js starter application for Bluemix
-//------------------------------------------------------------------------------
-
-// This application uses express as its web server
-// for more info, see: http://expressjs.com
-var express = require('express');
-
-// cfenv provides access to your Cloud Foundry environment
-// for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
-
-// create a new express server
 var app = express();
 
-// serve the files out of ./public as our main files
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname,'public')));
+app.use('/style', express.static(path.join(__dirname, '/views/style')));
+
+
+
 app.use(express.static(__dirname + '/public'));
 
-// get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
 
-// start server on the specified port and binding host
+
+
+function getDBCredentialsUrl(jsonData) {
+    var vcapServices = JSON.parse(jsonData);
+    // Pattern match to find the first instance of a Cloudant service in
+    // VCAP_SERVICES. If you know your service key, you can access the
+    // service credentials directly by using the vcapServices object.
+    for (var vcapService in vcapServices) {
+        if (vcapService.match(/cloudant/i)) {
+            return vcapServices[vcapService][0].credentials.url;
+        }
+    }
+}
+
+function initDBConnection() {
+    //When running on Bluemix, this variable will be set to a json object
+    //containing all the service credentials of all the bound services
+    if (process.env.VCAP_SERVICES) {
+        dbCredentials.url = getDBCredentialsUrl(process.env.VCAP_SERVICES);
+    } else { 
+        dbCredentials.url = getDBCredentialsUrl(fs.readFileSync("vcap-local.json", "utf-8"));
+    }
+
+    cloudant = require('cloudant')(dbCredentials.url);
+
+    // check if DB exists if not create
+    cloudant.db.create(dbCredentials.dbName, function(err, res) {
+        if (err) {
+            console.log('Could not create new db: ' + dbCredentials.dbName + ', it might already exist.');
+        }
+    });
+
+    db = cloudant.use(dbCredentials.dbName);
+}
+
+//initDBConnection();
+
+app.get('/', routes.chat);
+
+
+
+// load local VCAP configuration
+var vcapLocal = null
+if (require('fs').existsSync('./vcap-local.json')) {
+    try {
+        vcapLocal = require("./vcap-local.json");
+        console.log("Loaded local VCAP", vcapLocal);
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
+
+// get the app environment from Cloud Foundry, defaulting to local VCAP
+var appEnvOpts = vcapLocal ? {
+    vcap: vcapLocal
+} : {}
+var appEnv = cfenv.getAppEnv(appEnvOpts);
+var appName;
+if (appEnv.isLocal) {
+    require('dotenv').load();
+}
+
+
+
+
+
+
+// =====================================
+// WATSON CONVERSATION FOR ANA =========
+// =====================================
+app.post('/api/watson', function (req, res) {
+    processChatMessage(req, res);
+}); // End app.post 'api/ana'
+function processChatMessage(req, res) {
+    chatbot.sendMessage(req, function (err, data) {
+        if (err) {
+            console.log("Error in sending message: ", err);
+            res.status(err.code || 500).json(err);
+        }
+        else {
+//            Logs.find({
+//                selector: {
+//                    'conversation': data.context.conversation_id
+//                }
+//            }, function (err, result) {
+//                if (err) {
+//                    console.log("Cannot find log for conversation id of ", data.context.conversation_id);
+//                }
+//                else if (result.docs.length > 0) {
+//                    var doc = result.docs[0];
+//                    console.log("Sending log updates to dashboard");
+                    //console.log("doc: ", doc);
+//                    io.sockets.emit('logDoc', doc);
+//                }
+//                else {
+//                    console.log("No log file found.");
+//                }
+//            });
+            var context = data.context;
+//            var owner = req.user.username;
+            res.status(200).json(data);
+        }
+    });
+}
+
+
 app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
